42a30291971a458ae550ea27bb017eee7b47e05c
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-31 19:57:39 -0500

    update

--- src/main/java/com/template/TemplateReactSpringApplication.java
+++ src/main/java/com/template/TemplateReactSpringApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class TemplateReactSpringApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateReactSpringApplication.class, args);
 	}
-
 }
